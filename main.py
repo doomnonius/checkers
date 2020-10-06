f0af0f7bc16e28683ed8d769b43c68e1d6c98b1a
doomnonius <devon.doornbos@gmail.com>
2020-10-06 04:59:10 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
+import pygame
+from checker import WIDTH, HEIGHT, Board
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+def main():
+    """ Main loop
+    """
+    
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            
+            if event.type == pygame.KEYDOWN:
+                run = False
+    
+        board.draw_squares(WIN)
+        pygame.display.update()
+    
+    pygame.quit()
+
+main()
